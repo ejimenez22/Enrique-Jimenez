74624be504cc08dcf081fb1699d13b1a371ec10b
ejimenez22 <ejimenez2292@gmail.com>
2022-06-15 18:19:00 -0500

    set up files for components

---
+++ portfolio/src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+    return (
+        <section>
+            <h1>About Me</h1>
+            <p>This is a placeholder!</p>
+        </section>
+    )
+}
+
+export default About
