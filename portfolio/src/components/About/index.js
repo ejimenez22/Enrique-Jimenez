0d24d0e734cef16f9d682761bf7ba14ed3f2222c
ejimenez22 <ejimenez2292@gmail.com>
2022-06-17 15:01:03 -0500

    added resume pdf, but not displaying. Also made folders for projects and images

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <section>
             <h1>About Me</h1>
-            <p>This is a placeholder!</p>
+            <p>Software Engineer adept in all stages of advanced web development. Knowledgeable in the user interface, testing, and debugging processes. Equipped with a diverse and promising skill-set. Proficient in an assortment of technologies, including JavaScript,  REACT,  MERN, and MySQL. Able to effectively self-manage during independent projects and collaborate in a team setting.</p>
         </section>
     )
 }
--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Document, Page } from 'react-pdf'
 
 function Resume() {
+
+    const [ numPages, setNumPages] = useState(null)
+    const [pageNumber, setPageNumber] = useState(1)
+
+    function onDocumentLoad({numPages}) {
+        setNumPages(numPages)
+    }
     return (
         <section>
            <h1>My Resume</h1>
-           <img alt=""></img>
+           <Document file='Enrique Jimenez Developer Resume.pdf' onLoadSuccess={onDocumentLoad}>
+            <Page pageNumber={setPageNumber} />
+            </Document>
+            <p>
+                Page {pageNumber} of {numPages}
+            </p>
            <ul>
             <li>JavaScript</li>
             <li>React</li>
