9881482cc9f97272dae4e63e19801e8a4fefa65e
ejimenez22 <ejimenez2292@gmail.com>
2022-06-17 14:12:31 -0500

    added a template css, modifying as needed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import About from './components/About';
+import SideNav from './components/Nav';
+import './index.css'
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
+const [wid, setWid] = useState('0%')
+
+const openSideNav = () => {
+  setWid('15%')
+}
+
+const closeSidenav = () => {
+  setWid('0%')
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={openSideNav}>Open</button>
+      <SideNav name='Enrique' width={wid} closeNav={closeSidenav} />
+      <main>
+        <About></About>
+        <Portfolio></Portfolio>
+        <Contact></Contact>
+        <Resume></Resume>
+      </main>
     </div>
   );
 }
