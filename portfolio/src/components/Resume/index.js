84b1323e56ebd54371fe9bf785f3b31f266587e4
ejimenez22 <ejimenez2292@gmail.com>
2022-06-17 19:14:39 -0500

    added logos for skills

--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -11,17 +11,54 @@ function Resume() {
     }
     return (
         <section>
-           <h1>My Resume</h1>
-           <Document file='Enrique Jimenez Developer Resume.pdf' onLoadSuccess={onDocumentLoad}>
-            <Page pageNumber={setPageNumber} />
+            <h1>My Resume</h1>
+            <Document file='Enrique Jimenez Developer Resume.pdf' onLoadSuccess={onDocumentLoad}>
+                <Page pageNumber={setPageNumber} />
             </Document>
             <p>
                 Page {pageNumber} of {numPages}
             </p>
-           <ul>
-            <li>JavaScript</li>
-            <li>React</li>
-           </ul>
+            <div>
+                <h1> My Skills </h1>
+                <ul>
+                    <li>
+                        <img alt="js" src="../../src/assets/images/js.png" />
+                    </li>
+                    <li>
+                        <img alt="html" src="../../src/assets/images/HTML.png" />
+                    </li>
+                    <li>
+                        <img alt="css" src="../../src/assets/images/CSS.png" />
+                    </li>
+                    <li>
+                        <img alt="Bootstrap" src="../../src/assets/images/Bootstrap.png" />
+                    </li>
+                    <li>
+                        <img alt="Bulma" src="../../src/assets/images/Bulma.png" />
+                    </li>
+                    <li>
+                        <img alt="Git" src="../../src/assets/images/Git.png" />
+                    </li>
+                    <li>
+                        <img alt="jQuery" src="../../src/assets/images/Jquery.png" />
+                    </li>
+                    <li>
+                        <img alt="mongodb" src="../../src/assets/images/mongodb.png" />
+                    </li>
+                    <li>
+                        <img alt="MySQL" src="../../src/assets/images/MySQL.png" />
+                    </li>
+                    <li>
+                        <img alt="Node" src="../../src/assets/images/Node.png" />
+                    </li>
+                    <li>
+                        <img alt="React" src="../../src/assets/images/React.png" />
+                    </li>
+                    <li>
+                        <img alt="Rest" src="../../src/assets/images/REST.png" />
+                    </li>
+                </ul>
+            </div>
         </section>
     )
 }
