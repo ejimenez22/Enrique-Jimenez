3d09d5516d4b5ac2dc2934ba8f56849f76153d95
Enrique Jimenez <ejimenez2292@gmail.com>
2022-06-25 19:47:06 -0500

    working on 3d animated cube

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SideNav from "../Nav";
-//import {Link} from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faNode, faHtml5, faCss3, faReact, faJsSquare, faGithub} from '@fortawesome/free-brands-svg-icons'
 
 function Home() {
     return (
@@ -9,6 +10,28 @@ function Home() {
         <div className="homePage">
             <h1>Enrique Jimenez</h1>
             <h2>Software Engineer</h2>
+        </div>
+        <div className='cubeContainer'>
+            <div className="cube" id="boxspin">
+                <div className="face1">
+                    <FontAwesomeIcon icon={faNode} color='#006400' />
+                </div>
+                <div className="face2">
+                    <FontAwesomeIcon icon={faHtml5} color='#F06529' />
+                </div>
+                <div className="face3">
+                    <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+                </div>
+                <div className="face4">
+                    <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+                </div>
+                <div className="face5">
+                    <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+                </div>
+                <div className="face6">
+                    <FontAwesomeIcon icon={faGithub} color='#EC4D28' />
+                </div>
+            </div>
         </div>
         </>
     )
